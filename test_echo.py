62736e898fe3f52d4d8b4d619bed79da6dee5a17
Lazarev_K_E <klik_1995@yahoo.com>
2026-06-12 06:55:23 +0300

    намеренно нарушил параметры запроса при тестировании

--- test_echo.py
+++ test_echo.py
@@ -10,8 +10,7 @@ def test_get_with_query_params():
     assert response.status_code == 200
     data = response.json()
     
-    # PostmanEcho возвращает параметры как строки
-    expected_params = {"search": "python testing", "page": "5", "sort": "desc"}
+    expected_params = {"search": "python testing", "page": "999", "sort": "desc"}  # ← 999 вместо 5
     assert data["args"] == expected_params
 
 
